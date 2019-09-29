e05fdbc529ca5e7f080174fa30e969c09a1ef1d6
Devin Shi <yutian.shi@foxmail.com>
2019-09-29 19:01:00 +0800

    Update upload-img.js

--- src/js/editor/upload/upload-img.js
+++ src/js/editor/upload/upload-img.js
@@ -33,7 +33,7 @@ UploadImg.prototype = {
     },
 
     // 根据链接插入图片
-    insertLinkImg: function (link) {
+    insertLinkImg: function (link, id, dataTag) {
         if (!link) {
             return
         }
@@ -52,7 +52,7 @@ UploadImg.prototype = {
             }
         }
 
-        editor.cmd.do('insertHTML', `<img src="${link}" style="max-width:100%;"/>`)
+        editor.cmd.do('insertHTML', `<img id="${id}" data-tag="${dataTag}" src="${link}" style="max-width:100%;"/>`)
 
         // 验证图片 url 是否有效，无效的话给出提示
         let img = document.createElement('img')
